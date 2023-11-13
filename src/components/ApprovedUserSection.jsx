dec19d282bae95e3cb5c6c8d13e1660af6b81457
stastnan <nik.stastna@gmail.com>
2023-11-13 15:52:09 +0100

    Added user's board games library cards

--- src/components/ApprovedUserSection.jsx
+++ src/components/ApprovedUserSection.jsx
@@ -1,18 +1,76 @@
-import { Box, Card, Flex, Heading, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Flex,
+  Heading,
+  Image,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
+import { useState } from "react";
 import { customTheme } from "src/main";
 
 function ApprovedUserSection({ userGames, user }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedGame, setSelectedGame] = useState("");
+
+  const handleCardClick = (id) => {
+    const selectedGame = userGames.find((game) => game["@_objectid"] === id);
+    setIsOpen(true);
+    setSelectedGame(selectedGame);
+  };
+
   return (
-    <Flex direction="column" justify="center" align="center" gap="5" px="auto"  
-    w="100%"
-    bgGradient={`linear-gradient(0deg, black 0%, ${customTheme.colors.darkBrown} 68%)`}
-  >
-      <Heading color="white" py="5">{`This is a personal boardgames shelf of ${user}`}</Heading>
-      <Flex wrap="wrap" gap="10" justify="center" align="center">
+    <Flex
+      direction="column"
+      justify="center"
+      align="center"
+      gap="5"
+      px="auto"
+      w="100%"
+      bgGradient={`linear-gradient(0deg, black 0%, ${customTheme.colors.darkBrown} 68%)`}
+    >
+      <Heading
+        color="white"
+        py="5"
+      >{`This is a personal boardgames shelf of ${user}`}</Heading>
+      <SimpleGrid
+        spacing={5}
+        templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+      >
         {userGames?.map((game) => (
-          <Card maxW="150" maxH="350" key={game["@_objectid"]}>{game.name["#text"]} <Image src={game.thumbnail} /></Card>
+          <Card align="center" key={game["@_objectid"]}>
+            <CardHeader>
+              <Heading
+                align="center"
+                as="h3"
+                fontSize={{
+                  base: "xxs",
+                  sm: "sm",
+                  md: "md",
+                  lg: "lg",
+                }}
+              >
+                {game.name["#text"]}
+              </Heading>
+            </CardHeader>
+            <CardBody display="flex" align="center" justify="center">
+              <Image
+                src={game.thumbnail}
+                objectFit="scale-down"
+                onClick={handleCardClick}
+              />
+            </CardBody>
+            <CardFooter>
+              <Button>View here</Button>
+            </CardFooter>
+          </Card>
         ))}
-      </Flex>
+      </SimpleGrid>
     </Flex>
   );
 }
--- src/components/UserSection.jsx
+++ src/components/UserSection.jsx
@@ -1,6 +1,5 @@
 import { Box, Flex, HStack, Heading, IconButton, Text } from "@chakra-ui/react";
 import { UnlockIcon } from "@chakra-ui/icons";
-
 import { customTheme } from "src/main";
 import UserInput from "src/components/ui/UserInput";
 import { useEffect, useState } from "react";
@@ -10,7 +9,7 @@ import ApprovedUserSection from "./ApprovedUserSection";
 
 function UserSection() {
   const [user, setUser] = useState("");
-  const [userName, setUserName] = useState("")
+  const [userName, setUserName] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [userGames, setUserGames] = useState();
 
@@ -21,8 +20,6 @@ function UserSection() {
   }, [user]);
 
   function loadApprovedUserSection(userName) {
-
-
     if (userName) {
       setUser(userName);
       console.log(user);
@@ -55,39 +52,42 @@ function UserSection() {
 
   return (
     <>
-    <Box
-      as="section"
-      w="100%"
-      
-      bgGradient={`linear-gradient(0deg, ${customTheme.colors.darkBrown} 0%, ${customTheme.colors.lightBrown} 68%)`}
-      p="4"
-    >
-      <Flex w="100%" align="center" justify="center" direction="column" pt="10">
-        <Heading size="xl">Player Section</Heading>
-        <Text align="center" fontSize={{ base: "sm", sm: "md" }}>
-          Forge your personal saga within the realm of legendary board games. To
-          summon your gaming chronicles into our mystical archive, invoke your
-          BoardGameGeek username:
-        </Text>
-        <HStack py="10" spacing="3">
-          <UserInput
-            placeholder="Your BGG username"
-            setUserName={setUserName}
-            user={user}
-          />
-          <IconButton
-            variant="solid"
-            colorScheme="gray"
-            icon={<UnlockIcon />}
-            onClick={loadApprovedUserSection}
-          />
-        </HStack>
-      </Flex>
-     
-    </Box>
-     {user &&
-        <ApprovedUserSection userGames={userGames} user={user} />}
-</>
+      <Box
+        as="section"
+        w="100%"
+        bgGradient={`linear-gradient(0deg, ${customTheme.colors.darkBrown} 0%, ${customTheme.colors.lightBrown} 68%)`}
+        p="4"
+      >
+        <Flex
+          w="100%"
+          align="center"
+          justify="center"
+          direction="column"
+          pt="10"
+        >
+          <Heading size="xl">Player Section</Heading>
+          <Text align="center" fontSize={{ base: "sm", sm: "md" }}>
+            Forge your personal saga within the realm of legendary board games.
+            To summon your gaming chronicles into our mystical archive, invoke
+            your BoardGameGeek username:
+          </Text>
+          <HStack py="10" spacing="3">
+            <UserInput
+              placeholder="Your BGG username"
+              setUserName={setUserName}
+              user={user}
+            />
+            <IconButton
+              variant="solid"
+              colorScheme="gray"
+              icon={<UnlockIcon />}
+              onClick={loadApprovedUserSection}
+            />
+          </HStack>
+        </Flex>
+      </Box>
+      {user && <ApprovedUserSection userGames={userGames} user={user} />}
+    </>
   );
 }
 
